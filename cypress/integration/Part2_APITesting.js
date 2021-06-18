896f05c0c9746cbca2eb497fb7fbbceafd16d240
Ketnirun <pketnirun@sscinc.com>
2021-06-18 13:02:59 +0700

    added api tests

--- cypress/integration/Part2_APITesting.js
+++ cypress/integration/Part2_APITesting.js
@@ -1,6 +1,69 @@
 // Part2_APITesting.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+describe('POST User', function() {
+  it('POST User success', function() {
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('api_env') +'/auth',
+      headers: {
+        'accept': 'application/json',
+        'access_token': 'string'//input Token
+      },
+      body: {
+        username: "userapi1",
+        password:  "psswdapi1"
+      }
+    })
+      .its('status')
+      .should('equal',200)
+      })
+})
+
+describe('Get Orders', function() {
+  it('Get all Orders', function() {
+    cy.request({
+      method: 'GET',
+      url: Cypress.env('api_env') +'/orders'
+    }).as('Orders')
+    cy.get('@Orders').should((response) => {
+      assert.equal(response.status,200)
+      expect(response.body).to.not.be.null
+    })
+  })
+})
+
+describe('POST Orders', function() {
+  it('POST Orders success', function() {
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('api_env') +'/orders',
+      headers: {
+        'accept': 'application/json',
+        'order': "string"//input Token
+      },
+      body:{
+       "Crust": "NORMAL",
+       "Flavor": "CHEESE",
+       "Size": "M",
+       "Table_No": 6,
+       "Timestamp": "2021-06-18T18:21:08.708470"
+      }
+    })
+      .its('status')
+      .should('equal',200)
+      })
+})
+
+describe('Delete Order', function() {
+  it('DELETE Order fail', function() {
+    cy.request({
+      method: 'DELETE',
+      url: Cypress.env('api_env') +'/orders/1',//DELETE Order_ID 1
+      headers: {
+        'accept': 'application/json'
+      }
+    }).as('Order')
+      cy.get('@Order').should((response) => {
+      assert.equal(response.status,200)
+    })
+  })
+})
